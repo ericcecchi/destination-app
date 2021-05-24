69c809411991158b2abe232c445dcc2eeb5c28a1
Eric Cecchi <eric.cecchi@arcadia.com>
2021-05-24 17:25:56 -0500

    implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
   before_save { email.downcase! }
@@ -11,13 +13,45 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-  # Returns the hash digest of the given string.
-  def self.digest(string)
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
+  end
+
+  # Returns a session token to prevent session hijacking.
+  # We reuse the remember digest for convenience.
+  def session_token
+    remember_digest || remember
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,7 +10,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
       get root_path
       assert_select 'title', full_title
       assert_template 'static_pages/home'
-      assert_select 'a[href=?]', root_path, count: 2
+      assert_select 'a[href=?]', root_path, count: 3
       assert_select 'a[href=?]', help_path
       assert_select 'a[href=?]', about_path
       assert_select 'a[href=?]', contact_path
@@ -21,6 +21,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test 'signup' do
     get signup_path
-    assert_select 'title', full_title('Signup')
+    assert_select 'title', full_title('Sign up')
   end
 end
