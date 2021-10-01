af457e6e11441df48bc8f0c4f0069ff7d9fc0401
Eric Cecchi <eric.cecchi@arcadia.com>
2021-09-30 22:39:42 -0500

    add title to recommendations

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,8 +1,9 @@
 class Recommendation < ApplicationRecord
-  belongs_to :user
-  belongs_to :place
+  belongs_to :user, required: true
+  belongs_to :place, required: false
 
   validates :user_id, presence: true
+  validates :title, presence: true
   validates :content, presence: true
 
   default_scope -> { order(created_at: :desc) }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_174205) do
+ActiveRecord::Schema.define(version: 2021_09_28_031238) do
 
   create_table "locales", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_09_17_174205) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "place_id"
+    t.string "title"
     t.index ["place_id"], name: "index_recommendations_on_place_id"
     t.index ["user_id", "created_at"], name: "index_recommendations_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_recommendations_on_user_id"
