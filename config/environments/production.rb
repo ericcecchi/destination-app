2415abb76e005e7a1e087728be3a63477dd342b0
Eric Cecchi <eric.cecchi@arcadia.com>
2021-10-21 21:24:44 -0500

    update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  host = 'warm-depths-61226.herokuapp.com'
+  host = 'destination-app-production.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
