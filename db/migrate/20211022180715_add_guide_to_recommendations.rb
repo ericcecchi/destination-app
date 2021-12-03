fb2de99f3e17dc7a1b651dccedb6827dbaf3a7e6
Eric Cecchi <eric.cecchi@arcadia.com>
2021-12-03 12:19:14 -0600

    remove broken migrations

--- db/migrate/20211022180715_add_guide_to_recommendations.rb
+++
@@ -1,5 +0,0 @@
-class AddGuideToRecommendations < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :recommendations, :guide, null: false, foreign_key: true
-  end
-end
--- db/migrate/20211022184628_remove_guide_from_recommendations.rb
+++
@@ -1,8 +0,0 @@
-class RemoveGuideFromRecommendations < ActiveRecord::Migration[6.1]
-  def up
-    remove_foreign_key :recommendations, :guides
-    remove_column :recommendations, :guide_id
-  end
-
-  def down; end
-end
