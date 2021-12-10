895077ed93b7c28022c002bd2a080250753f0736
Zachary Collazo <ztcollazo08@gmail.com>
2021-12-10 13:03:34 -0500

    View guides for a locale.

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -5,4 +5,9 @@ class LocalesController < ApplicationController
   def index
     @locales = Locale.order('name').paginate(page: params[:page], per_page: 35)
   end
+
+  def show
+    @locale = Locale.find(params[:id])
+    @guides = @locale.guides.paginate(page: params[:page], per_page: 35)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/cities',     to: 'locales#index'
-  resources :recommendations
   root   'static_pages#home'
   get    '/about',   to: 'static_pages#about'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :locales, only: %i[index show], as: 'cities', path: '/cities'
+  resources :recommendations
   resources :users
   resources :guides, only: %i[new create]
   resources :account_activations, only: %i[create edit]
