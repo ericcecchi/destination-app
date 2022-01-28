02e78dc93e592af6df5d1dd27eef5f07b338c023
Zachary Collazo <ztcollazo08@gmail.com>
2022-01-28 13:36:04 -0500

    Add image_url to guides

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -6,6 +6,7 @@
 #
 #  id          :bigint           not null, primary key
 #  description :text
+#  image_url   :string
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
