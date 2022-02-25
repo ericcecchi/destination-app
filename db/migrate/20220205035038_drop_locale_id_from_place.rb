b4b71df0f48ba78eb443171a1cde58102fd088c4
Eric Cecchi <eric.cecchi@arcadia.com>
2022-02-25 12:58:22 -0600

    fix rescue?

--- db/migrate/20220205035038_drop_locale_id_from_place.rb
+++ db/migrate/20220205035038_drop_locale_id_from_place.rb
@@ -5,7 +5,7 @@ class DropLocaleIdFromPlace < ActiveRecord::Migration[6.1]
       remove_foreign_key :guides, :destinations
       add_foreign_key :guides, :places, column: :destination_id
       drop_table :destinations
-    rescue PG::UndefinedColumn
+    rescue PG::Error
       remove_column :places, :locale_id
       remove_foreign_key :guides, :locales
       add_foreign_key :guides, :places, column: :locale_id
